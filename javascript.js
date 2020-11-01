7cd2262efa74fbaaa505cff461892533a6e1ad2c
Alastair94 <szondiazazel@gmail.com>
2020-11-01 16:10:58 +0100

    Added headline selection
    - Copied in the data.txt
    - Created the HTML code for headline selecton + buttons
    -  Added comments to index.html + style.css
    - Added javascript function for text upload
    - Changed style for main page

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 var collapseBtn = document.getElementById('collapseBtn');
 var sidenav = document.getElementById('sidenav');
 var angle = document.getElementById('angle');
+var headline = document.getElementById('headline');
+
+var orgLabel = [];
+var orgSpecLabel = [];
+var recLabel = [];
+var recSpecLabel = [];
+var beginning = 0;
+var list;
 
 collapseBtn.addEventListener('click', function(){
 	sidenav.classList.toggle('active');
@@ -13,3 +21,35 @@ collapseBtn.addEventListener('click', function(){
 		angle.classList.toggle("fa-angle-right", false);
 	}
 });
+
+function fetchFile(){
+	list = [];
+	fetch("data.txt")
+		.then(res => res.text())
+		.then(content => content.split('\n'))
+		.then(lines => lines.map((part) => part.split('$$$')))
+		.then(splits => {
+			var index = 0;
+			splits.forEach(part => {
+				var data = {};
+				data.ID = index++;
+				data.recLabel = part[0];
+				data.recSpecLabel = part[1];
+				data.orgLabel = part[2];
+				data.headline = part[3];
+				data.content = part[4];
+				list.push(data);				
+			});
+			headline.innerHTML = "";
+			list.map((data, index) => {
+				if(index >= beginning && index < beginning + 20){
+					var dropdown = document.createElement('OPTION');
+					dropdown.innerText = data.headline;
+					dropdown.value = data.headline;
+					headline.appendChild(dropdown);
+				}
+			})
+		});
+};
+
+fetchFile();
